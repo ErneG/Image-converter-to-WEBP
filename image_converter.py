92c94e6e5d5dc8a373a19dc7067d56f702134536
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-12-12 21:45:41 +0200

    Update GUI for Manual Image Selection and Quality Specification
    - Enhanced the GUI to allow users to manually select images for conversion via a file dialog.
    - Added functionality to specify the conversion quality through an input field in the GUI.
    - The script now creates a default './OUTPUT' folder for storing the converted images.
    - Simplified the script by removing the watchdog-related code, focusing on manual conversion.
    - Ensured that the script checks for a valid image selection and quality input before conversion.

--- image_converter.py
+++ image_converter.py
@@ -1,22 +1,19 @@
 import os
-import shutil
 import datetime
-import threading
 import queue as Queue
 from PIL import Image as PILImage
 from tkinter import *
-from tkinter import scrolledtext
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from tkinter import filedialog, scrolledtext
 
 def get_new_filename(original_path, quality):
-    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")  # Compact timestamp
-    basename = os.path.splitext(os.path.basename(original_path))[0][:15]  # First 15 characters
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    basename = os.path.splitext(os.path.basename(original_path))[0][:15]
     return f"{timestamp}_{quality}_{basename}.webp"
 
 def convert_to_webp(image_path, output_path, quality, queue):
     try:
         with PILImage.open(image_path) as img:
+            ensure_folder_exists(output_path)
             new_filename = get_new_filename(image_path, quality)
             output_file = os.path.join(output_path, new_filename)
             img.convert('RGB').save(output_file, 'webp', quality=quality)
@@ -26,63 +23,49 @@ def convert_to_webp(image_path, output_path, quality, queue):
     except Exception as e:
         queue.put(f"Error converting {os.path.basename(image_path)}: {e}")
 
-
-        
-def read_config(file_path):
-    config = {}
-    with open(file_path, 'r') as file:
-        for line in file:
-            key, value = line.strip().split('=')
-            config[key] = value
-    return config
-
 def ensure_folder_exists(folder):
     if not os.path.exists(folder):
         os.makedirs(folder)
         print(f"Created folder: {folder}")
 
-def move_files_to_history(output_folder, history_folder):
-    for file in os.listdir(output_folder):
-        original_path = os.path.join(output_folder, file)
-        new_path = os.path.join(history_folder, file)
-        
-        # Check if the file already exists in the history folder
-        if os.path.exists(new_path):
-            # Create a new file name with a timestamp to prevent overwriting
-            timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-            basename, extension = os.path.splitext(file)
-            new_filename = f"{basename}_{timestamp}{extension}"
-            new_path = os.path.join(history_folder, new_filename)
-        
-        shutil.move(original_path, new_path)
-
-class WatchDirectory(FileSystemEventHandler):
-    def __init__(self, quality, output_folder, queue):
-        self.quality = quality
-        self.output_folder = output_folder
-        self.queue = queue
-
-    def on_created(self, event):
-        if not event.is_directory and event.src_path.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif')):
-            self.queue.put(f"Received event for: {event.src_path}")
-            convert_to_webp(event.src_path, self.output_folder, self.quality, self.queue)
-
-
 class GuiPart:
-    def __init__(self, master, queue, start_command, stop_command):
+    def __init__(self, master, queue):
         self.queue = queue
         self.master = master
-        self.observers = []  # List to keep track of observer threads
+        self.file_path = None
+        self.quality = StringVar(master, value='75')  # Default quality value
+
         master.title("Image Converter")
+
+        self.upload_button = Button(master, text="Upload Image", command=self.upload_image)
+        self.upload_button.pack(pady=5)
+
+        self.quality_label = Label(master, text="Quality (1-100):")
+        self.quality_label.pack(pady=5)
+
+        self.quality_entry = Entry(master, textvariable=self.quality)
+        self.quality_entry.pack(pady=5)
+
+        self.convert_button = Button(master, text="Convert", command=self.convert_image)
+        self.convert_button.pack(pady=5)
 
         self.text_area = scrolledtext.ScrolledText(master, wrap=WORD, height=10)
         self.text_area.pack(padx=10, pady=10, fill=BOTH, expand=True)
 
-        self.start_button = Button(master, text="Start", command=start_command)
-        self.start_button.pack(pady=(0, 10))
+    def upload_image(self):
+        self.file_path = filedialog.askopenfilename(
+            filetypes=[("Image Files", "*.jpeg;*.jpg;*.png;*.bmp;*.gif")]
+        )
+        if self.file_path:
+            self.text_area.insert(END, f"Selected file: {self.file_path}\n")
 
-        self.stop_button = Button(master, text="Stop", command=stop_command)
-        self.stop_button.pack(pady=(0, 10))
+    def convert_image(self):
+        if self.file_path and self.quality.get().isdigit():
+            quality_val = int(self.quality.get())
+            output_folder = './OUTPUT'  # Default output folder
+            convert_to_webp(self.file_path, output_folder, quality_val, self.queue)
+        else:
+            self.queue.put("Please select an image and set a valid quality value.")
 
     def process_incoming(self):
         while self.queue.qsize():
@@ -92,68 +75,10 @@ class GuiPart:
             except Queue.Empty:
                 pass
 
-    def on_window_close(self):
-        if self.observers:
-            for observer in self.observers:
-                observer.stop()
-            for observer in self.observers:
-                observer.join()
-        self.master.destroy()
-
-            
-def start_monitoring(queue):
-    config = read_config('config.txt')
-    output_folder = config['OUTPUT_FOLDER']
-    history_folder = config['HISTORY_FOLDER']
-    
-    ensure_folder_exists(output_folder)
-    ensure_folder_exists(history_folder)
-    move_files_to_history(output_folder, history_folder)
-
-    observers = []
-    for key, value in config.items():
-        if key.startswith('FOLDER_'):
-            quality = int(key.split('_')[1])
-            folder_to_watch = value
-            ensure_folder_exists(folder_to_watch)
-            event_handler = WatchDirectory(quality, output_folder, queue)
-            observer = Observer()
-            observer.schedule(event_handler, folder_to_watch, recursive=False)
-            observer.start()
-            observers.append(observer)
-
-    return observers
-
-
-class ThreadedTask(threading.Thread):
-    def __init__(self, queue):
-        threading.Thread.__init__(self)
-        self.queue = queue
-
-    def run(self):
-        start_monitoring(self.queue)
-
-class WatchDirectory(FileSystemEventHandler):
-    def __init__(self, quality, output_folder, queue):
-        self.quality = quality
-        self.output_folder = output_folder
-        self.queue = queue
-
-    def on_created(self, event):
-        if not event.is_directory and event.src_path.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif')):
-            self.queue.put(f"Received event for: {event.src_path}")
-            convert_to_webp(event.src_path, self.output_folder, self.quality, self.queue)
-            
-
 def main():
     root = Tk()
     queue = Queue.Queue()
-
-    def start_command():
-        gui.observers = start_monitoring(queue)
-
-    gui = GuiPart(root, queue, start_command, root.quit)
-    root.protocol("WM_DELETE_WINDOW", gui.on_window_close)
+    gui = GuiPart(root, queue)
 
     def periodic_call():
         gui.process_incoming()
@@ -161,8 +86,6 @@ def main():
 
     periodic_call()
     root.mainloop()
-
-
 
 if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -1,19 +1,17 @@
-# Image Converter Script with GUI
+# Image Converter Script with Enhanced GUI
 
-This script automatically converts images added to specific folders to WEBP format and saves them to an output folder using a unique naming convention. It now includes a Tkinter-based GUI for easier control and monitoring.
+This script now features an enhanced GUI that allows users to manually select images for conversion to WEBP format, specify the quality, and view conversion logs.
 
 ## Features
-- Monitors specified folders for new images and converts them to WEBP format.
+- Manually select images for conversion using a file dialog.
+- Specify the desired quality for the WEBP conversion (1-100).
+- Converted images are saved in a default './OUTPUT' folder.
 - Unique naming for converted images: '[YYYYMMDD_HHMMSS]_[quality]_[original_filename:15].webp'.
-- GUI for starting/stopping the monitoring process and viewing log messages.
-- Error handling for continuous operation and specific error messages for file access and image processing issues.
-
-## Configuration
-- Set source folders and quality settings in `config.txt`.
-- Specify the output and history folders in `config.txt`.
+- Real-time log messages displayed in the GUI, including conversion status and errors.
 
 ## How to Use
-- Adjust `config.txt` with folder paths and quality settings.
-- Run the script. The GUI will appear for control and monitoring.
-- Use the 'Start' button to begin monitoring and 'Stop' to end it.
-- View conversion logs and error messages directly in the GUI.
+- Run the script to open the GUI.
+- Click 'Upload Image' and select an image file.
+- Enter the desired quality for the WEBP conversion.
+- Click 'Convert' to start the conversion process.
+- View conversion status and any error messages in the text area of the GUI.
