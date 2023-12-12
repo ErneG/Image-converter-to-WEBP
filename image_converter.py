00f10cb76c7822589450ac754b10d94296109df6
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-12-12 02:28:22 +0200

    Implement multiple input folders with varying quality and an output folder
    - Modified the script to monitor multiple input folders, each associated with a different image quality setting.
    - Added functionality to save the converted images to a separate output folder.
    - Updated the configuration file to include multiple source folders and their respective quality settings, along with the output folder path.

--- image_converter.py
+++ image_converter.py
@@ -1,40 +1,60 @@
+import os
 from PIL import Image
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-def convert_to_webp(image_path, quality):
-    with Image.open(image_path) as img:
-        img.convert('RGB').save(image_path + '.webp', 'webp', quality=quality)
+def convert_to_webp(image_path, output_path, quality):
+    try:
+        with Image.open(image_path) as img:
+            output_file = os.path.join(output_path, os.path.basename(image_path) + '.webp')
+            img.convert('RGB').save(output_file, 'webp', quality=quality)
+            print(f"Successfully converted '{os.path.basename(image_path)}' to WEBP at '{output_path}' with quality {quality}.")
+    except Exception as e:
+        print(f"Error converting '{image_path}': {e}")
 
 def read_config(file_path):
     config = {}
+    base_path = os.path.dirname(os.path.abspath(file_path))
     with open(file_path, 'r') as file:
         for line in file:
             key, value = line.strip().split('=')
-            config[key] = value
+            abs_path = os.path.abspath(os.path.join(base_path, value))  # Convert to absolute path
+            config[key] = abs_path
     return config
 
+
 class WatchDirectory(FileSystemEventHandler):
-    def __init__(self, quality):
+    def __init__(self, quality, output_folder):
         self.quality = quality
+        self.output_folder = output_folder
 
     def on_created(self, event):
         if not event.is_directory and event.src_path.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif')):
-            convert_to_webp(event.src_path, self.quality)
+            print(f"Received task for '{event.src_path}'. Quality: {self.quality}")
+            convert_to_webp(event.src_path, self.output_folder, self.quality)
 
 if __name__ == "__main__":
     config = read_config('config.txt')
-    folder_to_watch = config['FOLDER_TO_WATCH']
-    quality = int(config['QUALITY'])
+    output_folder = config['OUTPUT_FOLDER']
+    observers = []
 
-    event_handler = WatchDirectory(quality)
-    observer = Observer()
-    observer.schedule(event_handler, folder_to_watch, recursive=False)
-    observer.start()
+    for key, value in config.items():
+        if key.startswith('FOLDER_'):
+            quality = int(key.split('_')[1])
+            folder_to_watch = value
+            event_handler = WatchDirectory(quality, output_folder)
+            observer = Observer()
+            observer.schedule(event_handler, folder_to_watch, recursive=False)
+            observer.start()
+            observers.append(observer)
+            print(f"Monitoring '{folder_to_watch}' for quality {quality}")
 
     try:
         while True:
             pass
     except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+        for observer in observers:
+            observer.stop()
+        for observer in observers:
+            observer.join()
+        print("Stopped all observers")
--- readme.md
+++ readme.md
@@ -1,12 +1,12 @@
 # Image Converter Script
 
-This script automatically converts new images added to a specified folder to WEBP format, based on settings defined in a `config.txt` file.
+This script monitors multiple folders, each with a specific quality setting, and converts new images in these folders to WEBP format. The converted images are saved to a separate output folder.
 
 ## Configuration
-- Set the `FOLDER_TO_WATCH` and `QUALITY` values in `config.txt`.
-- `FOLDER_TO_WATCH`: Path of the folder to monitor.
-- `QUALITY`: Quality of the converted WEBP images.
+- Define source folders and their respective qualities in `config.txt`.
+- Specify the output folder in `config.txt` (`OUTPUT_FOLDER`).
+- Each source folder name should be prefixed with `FOLDER_` followed by the desired quality.
 
 ## How to Use
-- Update the settings in `config.txt`.
-- Run the script. It will monitor the specified folder and convert new images to WEBP format.
+- Update the settings in `config.txt` to include your source folders and output folder.
+- Run the script. It will monitor the specified folders and convert new images to WEBP format.
